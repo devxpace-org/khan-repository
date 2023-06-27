c66e524a34a540c8ff7bcf4977b3d06b1eaa12ff
poornarallapalli <34799394+poornarallapalli@users.noreply.github.com>
2023-06-27 12:30:11 -0400

    Update helloworld.js

--- src/helloworld.js
+++ src/helloworld.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/helloworld', (req, res) => {
-  res.send('HELLO WORLD!, this is from container two at port 5000!')
+app.get('/nicejob', (req, res) => {
+  res.send('NICE JOB!, this is from container one at port 3000!')
 })
 
 app.listen(port, () => {
