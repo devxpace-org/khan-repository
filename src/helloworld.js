a344f2abb76434b362edf93c144a32fc2c499f81
poornarallapalli <34799394+poornarallapalli@users.noreply.github.com>
2023-06-29 10:15:23 -0400

    Update helloworld.js

--- src/helloworld.js
+++ src/helloworld.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/ronaldo', (req, res) => {
-  res.send('HELLO WORLD!, this is ronaldo!')
+app.get('/messi', (req, res) => {
+  res.send('HELLO WORLD!, this is messi!')
 })
 
 app.listen(port, () => {
